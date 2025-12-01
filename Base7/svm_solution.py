0cd091ed8314910840ff05778d28b9f94cb2ef7e
VinokurovKV <kirikvin8490@gmail.com>
2025-12-01 14:59:55 +0300

    ML for [Base][7] SVM

---
+++ Base7/svm_solution.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.svm import SVC
+
+
+def train_svm_and_predict(train_features, train_target, test_features):
+    """
+    train_features: np.array, (num_elements_train x num_features) - train data description, the same features and the same order as in train data
+    train_target: np.array, (num_elements_train) - train data target
+    test_features: np.array, (num_elements_test x num_features) -- some test data, features are in the same order as train features
+
+    return: np.array, (num_elements_test) - test data predicted target, 1d array
+    """
+    X_train_selected = train_features[:, 3:5]
+    X_test_selected = test_features[:, 3:5]
+
+    model_params = {
+        'C': 10,
+        'gamma': 0.1,
+        'kernel': 'rbf',
+        'class_weight': 'balanced'
+    }
+
+    model = SVC(**model_params)
+    model.fit(X_train_selected, train_target)
+
+    return model.predict(X_test_selected)
